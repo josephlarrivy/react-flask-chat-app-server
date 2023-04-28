ca618524a27682510c5b44e681ad9d7c4721af46
josephlarrivy <joseph.larrivy@gmail.com>
2023-04-28 09:01:00 -0500

    working around cors

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 app = Flask(__name__)
 # port = int(os.environ.get("PORT", 5000))
 # CORS(app)
-CORS(app, resources={r"/*": {"origins": "https://clearchat-client.herokuapp.com/"}})
+CORS(app, resources={r"/*": {"origins": '*'}})
 
 # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///chat-app-database"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql-cubic-26621"
